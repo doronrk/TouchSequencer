c4f3ab0b38b0663a7d88739146529ff257b3a581
doronrk <doronrk@gmail.com>
2015-10-18 20:20:59 -0700

    first commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,94 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    pad = ofxMultiTouchPad();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofEnableSmoothing();
+    
+    /*
+     Use the MTouch datatype for more information on touches
+     */
+    ofSetColor(255, 128, 0);
+    std::vector<MTouch> mTouches = pad.getTouches();
+    float scale = 100;
+    for (std::vector<MTouch>::iterator touch=mTouches.begin(); touch!=mTouches.end(); ++touch)
+    {
+        float size = touch->size*scale;
+        ofPushMatrix();
+        ofTranslate(touch->x*ofGetWidth(), touch->y*ofGetHeight());
+        ofRotate(touch->angle);
+        ofEllipse(0, 0, size, size*.5);
+        ofPopMatrix();
+    }
+    
+    
+    /*
+     Iterate over all touches as a vector of ofPoints Ð very simple datatype though...
+     */
+    ofPushMatrix();
+    ofSetColor(255, 255, 255);
+    pad.getTouchesAsOfPoints(&touches);
+    ofPoint size = ofPoint(ofGetWidth(),ofGetHeight(),0);
+    ofSetRectMode(OF_RECTMODE_CENTER);
+    for (vector<ofPoint>::iterator touch = touches.begin(); touch!=touches.end(); ++touch) {
+        ofRect((*touch)*size, 10, 10);
+    }
+    ofPopMatrix();
+    
+    string info = "Number of touches: "+ofToString(pad.getTouchCount(),0);
+    ofDrawBitmapStringHighlight(info, 20, 20);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
